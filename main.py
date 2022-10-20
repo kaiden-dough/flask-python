1e0076fc6eae5c5e075a54017be56f3da84c73e4
69410126 <kaiden12345do@gmail.com>
2022-10-19 20:41:21 -0700

    trfa

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import render_template  # import render_template from "public" flask libraries
 # import "packages" from "this" project
 from __init__ import app  # Definitions initialization
-from api import app_api # Blueprint import api definition
+from api.jokes import app_api # Blueprint import api definition
 from bp_projects.projects import app_projects # Blueprint directory import projects definition
 
 app.register_blueprint(app_api) # register api routes
